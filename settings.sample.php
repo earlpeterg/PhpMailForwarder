ff1307334d332fcacdacc9a65101bdc0b213e3aa
Earl Peter G <earl.gangoso@outlook.com>
2018-04-04 15:25:07 +0800

    Updated settings (removed unnecessary options and comments)

--- settings.sample.php
+++ settings.sample.php
@@ -9,25 +9,16 @@ $instances[] = [
 	'in' => [
 		'host' => 'imap.yourdomain.com',
 		'port' => 995,
-		'type' => '/imap', // can be: pop3 or imap, note: seen/unseen does not work on pop3
-		'security' => '/ssl' // can be: ssl, tls, notls
+		'type' => 'imap', // can be: pop3 or imap, note: seen/unseen does not work on pop3
+		'security' => 'ssl' // can be: ssl, tls, notls
 	],
-	'out' => [
-		// sends using php mail() func
-		'phpmail' => [
-			// when set to true, the source sender's email will be set as "from" email
-			'use_original_email' => true
-		]
-	],
-	
+
 	// username and password for incoming mail box
 	'user' => 'user@example.com',
 	'password' => 'password',
 	
 	// where to send
 	'to' => ['new_user@example', 'new_user_2@example.com'],
-	'cc' => [],
-	'bcc' => [],
 
 	'delete_copy' => false // when set to true message will be delete after forwarded
 ];
